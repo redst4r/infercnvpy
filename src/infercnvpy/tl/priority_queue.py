b2a8da4b6be38f92c817fdf78e5d81eaeb120ec9
redst4r <redst4r@web.de>
2022-10-03 12:54:39 -0700

    priority queue implementation of segmentor

---
+++ src/infercnvpy/tl/priority_queue.py
@@ -0,0 +1,192 @@
+from collections import namedtuple
+"""
+Priority Queue implemented via a binary Heap
+mstly from https://www.geeksforgeeks.org/priority-queue-using-binary-heap/
+"""
+
+Element = namedtuple("HeapElement", "priority datum")
+
+# Function to return the index of the
+# parent node of a given node
+def parent(i) :
+ 
+    return (i - 1) // 2
+   
+# Function to return the index of the
+# left child of the given node
+def leftChild(i) :
+ 
+    return ((2 * i) + 1)
+   
+# Function to return the index of the
+# right child of the given node
+def rightChild(i) :
+ 
+    return ((2 * i) + 2)
+     
+
+class PriorityHeap():
+    def __init__(self, max_size, verbose=False):
+        self.H = [Element(0, None) for _ in range(max_size)]  #[(0, None) for _ in range(max_size)]
+        self.size= -1
+        self.max_size = max_size  #TODO redundant wuth len(H)
+        self.verbose=verbose
+        
+    # Function to shift up the 
+    # node in order to maintain 
+    # the heap property
+    def shiftUp(self, i) :
+        assert self._exists(i), f"element at node {i} does not exist"
+        while (i > 0 and self.H[parent(i)].priority < self.H[i].priority) :
+
+            # Swap parent and current node
+            self.swap(parent(i), i)
+
+            # Update i to parent of i
+            i = parent(i)
+
+    # Function to shift down the node in
+    # order to maintain the heap property
+    def shiftDown(self, i) :
+        assert self._exists(i), f"element at node {i} does not exist"
+        maxIndex = i
+
+        # Left Child
+        l = leftChild(i)
+
+        if (l <= self.size and self.H[l].priority > self.H[maxIndex].priority) :
+
+            maxIndex = l
+
+        # Right Child
+        r = rightChild(i)
+
+        if (r <= self.size and self.H[r].priority > self.H[maxIndex].priority) :
+
+            maxIndex = r
+
+        # If i not same as maxIndex
+        if (i != maxIndex) :
+
+            self.swap(i, maxIndex)
+            self.shiftDown(maxIndex)
+
+    # Function to insert a 
+    # new element in 
+    # the Binary Heap
+    def insert(self, priority, datum) :
+        if self.verbose:
+            pass #print(f"HEAP\tInserting {datum} -- {priority}")
+        if self.size + 1 >= self.max_size:
+            raise ValueError(f"heap is at max capacity {self.max_size}")
+        self.size +=1 
+        self.H[self.size] = Element(priority, datum)
+
+        # Shift Up to maintain 
+        # heap property
+        self.shiftUp(self.size)
+
+    # Function to extract 
+    # the element with
+    # maximum priority
+    def extractMax(self) :
+        if self.size <1 :
+            raise ValueError("Heap empty")
+        result = self.H[0]
+
+        # Replace the value 
+        # at the root with 
+        # the last leaf
+        self.H[0] = self.H[self.size]
+        self.H[self.size] = Element(0, None) ## fill the old element
+        self.size -=  1
+
+        # Shift down the replaced 
+        # element to maintain the 
+        # heap property
+        self.shiftDown(0)
+        return result
+
+    # Function to change the priority
+    # of an element
+    def changePriority(self, i,p) :
+        assert self._exists(i), f"element at node {i} does not exist"
+        oldp = self.H[i].priority
+        
+        # doesnt work due to immutable tuples
+        #self.H[i].priority = p
+        self.H[i] = Element(p, self.H[i].datum)
+
+        if self.verbose:
+            print(f"HEAP\tUpdating {self.H[i].datum} -- {oldp} -> {p}")
+        
+        if (p > oldp) :
+            self.shiftUp(i)
+        else:
+            self.shiftDown(i)
+
+    # Function to get value of 
+    # the current maximum element
+    def getMax(self,) :
+
+        return self.H[0]
+
+    # Function to remove the element
+    # located at given index
+    def Remove(self, i) :
+        """
+        essetnaiylly, make it (artificially) the most important node, bubble it to the top and pop
+        """
+        assert self._exists(i), f"element at node {i} does not exist"
+        
+        # artificially increase priority
+        newP = self.getMax().priority + 1
+        
+        # since tuples are immutable this doesnt work:
+        #self.H[i].priority = newP
+        self.H[i] = Element(newP, self.H[i].datum)
+    
+        if self.verbose:
+            print(f"HEAP\tRemoving {self.H[i].datum}")
+#         print(f"removing {self.H[i]}")
+        
+        # Shift the node to the root
+        # of the heap
+        self.shiftUp(i)
+
+        # Extract the node
+        self.extractMax()
+
+    def swap(self, i, j) :
+
+        temp = self.H[i]
+        self.H[i] = self.H[j]
+        self.H[j] = temp
+        
+    def _exists(self, i):
+        if i > self.size:
+            return False
+        return Element(0, None) != self.H[i]
+    
+    
+    def search_element(self, datum):
+        """
+        descend the tree level by level,
+        """
+        current_level_nodes = [0]
+        while len(current_level_nodes) > 0:
+            next_level_nodes = []
+            for current_node in current_level_nodes:
+                if self.H[current_node].datum == datum:
+                    return current_node
+                
+                lC = leftChild(current_node)
+                rC = rightChild(current_node)
+                if self._exists(lC):
+                    next_level_nodes.append(lC)
+                if self._exists(rC):
+                    next_level_nodes.append(rC)
+            current_level_nodes = next_level_nodes
+        
+        raise ValueError(f"Element not found {datum}")
+
--- src/infercnvpy/tl/segmentation.py
+++ src/infercnvpy/tl/segmentation.py
@@ -1,7 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 class PWC_Segmentor():
     
     def __init__(self, signal: np.ndarray):
@@ -90,6 +89,200 @@ class PWC_Segmentor():
             Lmin = lambda_vec[_merge_ix]  # the cost of mergin
             # if the cost if smaller than our current threshold, do merge
             if Lmin < current_lambda:
+                self._merge_interval(_merge_ix)
+            else:
+                lambda_schedule.append(current_lambda)
+                current_lambda = Lmin + 1e-6
+            
+                # check if we converged
+                deltaL = current_lambda - lambda_schedule[-1]
+                if deltaL > beta * v: #NOTE small delta values are GOOD, meaning that we're still joining meaninful regions
+                    break
+        return lambda_schedule
+
+    def get_segmented_signal(self):
+
+        mean_values = []
+        for i in range(len(self.interval_start)):
+            m = self._get_interval(i).mean()
+            mean_values.extend([m]*self._get_interval_length(i))
+        mean_values = np.array(mean_values)
+        assert len(mean_values) == len(self.signal)
+        return mean_values
+
+from .priority_queue import Priority
+class PWC_Segmentor_Heap():
+
+    def __init__(self, signal: np.ndarray, verbose=False):
+        """
+        signal: 1D signal to get segmented
+        """
+        assert len(signal.shape) == 1, "signal needs to be 1D. flatten it?"
+        # each interval is represented by its start and end
+        # i.e. interval[i] has interval_start[i] and interval_end[i]
+        self.heap = PriorityHeap(max_size=len(signal), verbose=verbose)
+        self.signal = signal 
+        self.verbose = verbose
+        self.interval_start= np.arange(0, len(signal))
+        self.interval_end = np.arange(1, len(signal)+1)
+        self.interval_id = np.array([f"I_{_}" for _ in np.arange(0, len(signal))])  # by convention, if we merge two intervals, we keep the name of the left
+        
+        for i in self.interval_start[:-1]:  # the last/most-right interval cant be merged, notthing right of it!
+            priority = (-1) * self._lambda_hat(i)  # -1 for working with a maxHeap
+            self.heap.insert(priority, f'I_{i}')
+            
+                                 
+    def _get_interval(self, i: int):
+        """
+        get the signal in the given interval
+        """
+        return self.signal[self.interval_start[i]:self.interval_end[i]]
+
+    def _get_interval_length(self, i):
+        """
+        get the length of the interval[i]
+        """
+        L = self.interval_end[i] - self.interval_start[i]
+        assert L == len(self._get_interval(i))
+        return L
+
+    def _merge_interval(self, left_ix):
+        """
+        merge two adajacent intervals
+        Convention: when merging, we always merge the left to the right
+        i.e. we index the breakpoint by the end of the left interval
+        
+        assume a layout like this 1|2|3|4  and a merge of 2 -> 1|2  |4
+        
+        after the merge, we have to update the cost/priority
+        - the interval[left_index] gets new cost (potentially merging it to 4 )
+        - the interval to the left (1 in the exmaple) also needs updating
+        - interval 3 (being merged to 2) will be removed entirely
+        """
+        right_ix = left_ix + 1
+    
+        # now, for the interaction with the HEAP, where we need the interval names
+        # important: do this BEFORE deleting anything in self.interval_ids
+        right_ID = self.interval_id[right_ix]
+        left_ID = self.interval_id[left_ix]        
+
+        
+        # set the left-interval's end to the right-intervals end 
+        self.interval_end[left_ix] = self.interval_end[right_ix]
+        
+        
+        # delete the right interval from the heap
+        # there's one weird exception: Merging into the rightmost interval, i.e. 1|2|3|4
+        # merging 3|4
+        # the rightmost interval (4) is NOT on the heap (since it cant be merged to the right)
+        # its still a valid merge (the interval exists)
+        # the new merged interval 3 (1|2|3) is now the new rightmost interval
+        # - remove it from the stack, it has no priority
+        # TODO this is really UGLY, with the order of deleting the array etc!!!
+        if right_ID == self.interval_id[-1]:
+            # no need to delete the right itnerval from the heap
+            # remove the left, as it will never be merged
+            heap_ix = self.heap.search_element(left_ID)
+            self.heap.Remove(heap_ix)
+            
+            # delete the right interval from the array
+            # note the palcement of this code. AFTER THIS, the right_ix wont be a valid address any more!
+            # the left_ix doesnt change
+            self.interval_start = np.delete(self.interval_start, right_ix)
+            self.interval_end = np.delete(self.interval_end, right_ix)
+            self.interval_id = np.delete(self.interval_id, right_ix)
+            del right_ix, right_ID
+            
+            # no need to update the left priority, its not on the heap anymore
+            
+        else:
+            heap_ix = self.heap.search_element(right_ID)
+            self.heap.Remove(heap_ix)
+        
+        
+            # delete the right interval from the array
+            # note the palcement of this code. AFTER THIS, the right_ix wont be a valid address any more!
+            # the left_ix doesnt change
+            self.interval_start = np.delete(self.interval_start, right_ix)
+            self.interval_end = np.delete(self.interval_end, right_ix)
+            self.interval_id = np.delete(self.interval_id, right_ix)
+            del right_ix, right_ID
+
+            # update the left interval on the heap, its priority changed
+            newPriority = (-1) * self._lambda_hat(left_ix)  # -1 to make it work with a max heap
+            # not that it's boundaries changed too, but the name stays the same!! no need to change the item at all
+            heap_ix = self.heap.search_element(left_ID)
+            self.heap.changePriority(heap_ix, newPriority)      
+        
+        # update the interval LEFT of left_ix (it got new priority too)
+        # if left_ix ==0 (already the leftmost), skip
+        if left_ix >0:
+            leftleft_ix = left_ix - 1
+            leftleft_id = self.interval_id[leftleft_ix]
+            heap_ix = self.heap.search_element(leftleft_id)
+            
+            newPriority = (-1) * self._lambda_hat(leftleft_ix)  # -1 to make it work with a max heap
+            self.heap.changePriority(heap_ix, newPriority)      
+            
+        
+        
+    def _lambda_hat(self, left_ix):
+        """
+        calculate the cost of merging the left interval (to its right)
+        """
+        # breakpoint is the end of the left interval
+        right_ix = left_ix + 1
+        
+        R_im1 = self._get_interval_length(left_ix)
+        R_i = self._get_interval_length(right_ix)
+        U_im1 = self._get_interval(left_ix).mean()
+        U_i = self._get_interval(right_ix).mean()
+        
+        l = ((U_im1 - U_i)**2)*(R_im1 * R_i) / (R_im1 + R_i)
+        
+        return l
+
+    def plot_current(self):
+        """
+        plot the segmentation over the signal
+        """
+        seg_signal = self.get_segmented_signal()
+        plt.plot(np.arange(len(self.signal)), seg_signal)
+        plt.plot(np.arange(len(self.signal)), self.signal)
+
+
+    def do_segmentation(self, iterations=10000, beta=0.5):
+        """
+        run the actual segmentation for a given number of iterations or until we hit the stop criterion
+        :return: 
+            the schedule of lambda (how its increasing with iterations). 
+            When it starts increasing too much, we cant find good merges, and are probably done
+        """
+        # standard dev of the data, used for the stoppoing criterion
+        v = np.std(self.signal)
+
+        # remember how lambda increases
+        lambda_schedule = [] 
+        current_lambda = 0
+        for i in range(iterations):
+            
+            # find the breakpoint with the smallest lambda
+            priority, the_ID = self.heap.getMax()
+            Lmin = (-1) * priority  ## again, maxHeap! high priorty -> low cost
+            
+            _merge_ix = np.where(self.interval_id == the_ID)[0][0]
+            
+            # sanity check, making sure Heap and data are in sync
+#             ptest = -self._lambda_hat(_merge_ix)
+#             np.testing.assert_allclose(priority, ptest, err_msg=f"Priorities dont match! {priority} vs {ptest}, node {the_ID}")
+            
+            
+            # if the cost if smaller than our current threshold, do merge
+            if Lmin < current_lambda:
+                iname = self.interval_id[_merge_ix]
+                if self.verbose:
+                    print(f'merging {iname}, ix {_merge_ix}')
+                    
                 self._merge_interval(_merge_ix)
             else:
                 lambda_schedule.append(current_lambda)
