c75dd4573c418f475db55b0cc9b9e4397afaabde
redst4r <redst4r@web.de>
2022-10-14 16:38:52 -0700

    importlib.metadata is only available in py3.7

--- src/infercnvpy/__init__.py
+++ src/infercnvpy/__init__.py
@@ -1,7 +1,8 @@
 """Python library to infer copy number variation (CNV) from single-cell RNA-seq data"""
-from importlib.metadata import version
+# from importlib.metadata import version
 
 from . import datasets, io, pl, pp, tl
 
 __all__ = ["datasets", "io", "pl", "pp", "tl"]
-__version__ = version("infercnvpy")
+# __version__ = version("infercnvpy")
+__version__ = "0.1"
